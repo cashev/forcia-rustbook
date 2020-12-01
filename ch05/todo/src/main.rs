1c9f0dec642416aebd6733e5cb8139effbd2fefc
cashew <42924021+cashev@users.noreply.github.com>
2020-12-01 22:47:53 +0900

    cargo fmtを適応

--- ch05/todo/src/main.rs
+++ ch05/todo/src/main.rs
@@ -42,9 +42,9 @@ async fn index() -> Result<HttpResponse, MyError> {
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
     HttpServer::new(move || App::new().service(index))
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await?;
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
 
     Ok(())
 }
