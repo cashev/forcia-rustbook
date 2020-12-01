a91cf48882eb345309e7ee23a167b119f5067f08
cashew <42924021+cashev@users.noreply.github.com>
2020-12-01 22:16:23 +0900

    エラーハンドリングを定義

--- ch05/todo/src/main.rs
+++ ch05/todo/src/main.rs
@@ -1,4 +1,17 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
+    let response_body = "Hello world!";
+
+    Ok(HttpResponse::Ok().body(response_body))
+}
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
@@ -8,11 +21,4 @@ async fn main() -> Result<(), actix_web::Error> {
     .await?;
 
     Ok(())
-}
-
-#[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello world!";
-
-    Ok(HttpResponse::Ok().body(response_body))
 }
